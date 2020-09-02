977f9c40e8b2c3db3b13c901ce3408835f424db2
Jordan <jordan.guy.277@gmail.com>
2020-09-01 20:01:19 -0400

    began mobile first design

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path'); //part of the Node library
 const postCSSPlugins = [
     require("postcss-import"),
+    require("postcss-mixins"),
     require('postcss-simple-vars'),
     require('postcss-nested'),
     require('autoprefixer')
